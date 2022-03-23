08ad382d12ba28c3d6094c3b6d770ed80a404a61
Jonasthj <jonasthenderupjensen@gmail.com>
2022-03-23 14:20:45 +0100

    display message from server on client

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
 import express from "express";
-import {WebSocketServer} from 'ws';
+import { WebSocketServer } from "ws";
 
 const app = express();
 
 app.use(express.static("../client/dist"));
 
-const wsServer = new WebSocketServer({noServer: true});
+const wsServer = new WebSocketServer({ noServer: true });
 wsServer.on("connect", (socket) => {
   console.log("WS CONNECTED");
-  socket.send("Hello");
+  socket.send(JSON.stringify({ author: "Server", message: "Heeello!" }));
 });
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`http://localhost:${server.address().port}`);
-  server.on('upgrade', (req, socket, head) => {
+  server.on("upgrade", (req, socket, head) => {
     wsServer.handleUpgrade(req, socket, head, (socket) => {
       wsServer.emit("connect", socket, req);
     });
-  })
+  });
 });
